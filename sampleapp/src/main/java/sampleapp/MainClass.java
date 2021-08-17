f25fc044bbcf16e97eb6edcd329f44d9e690cf17
R. Ramakrishnan <R. Ramakrishnan@LAPTOP-BFE84NAE>
2021-08-17 15:12:36 +0530

    First Commit

---
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -0,0 +1,10 @@
+package sampleapp;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Good Afternoon");
+		System.out.println("Have a Good Day!");
+		System.out.println("Welcome to Git !");
+	}
+}
